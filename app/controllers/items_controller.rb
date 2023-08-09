db99b7b766af63c65fb58d6faf5d6f37ab9793c0
Takagi <zkazo@gmx.com>
2023-08-09 19:58:10 +0900

    一覧に表示できた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    @items = Item.order(release: :desc).limit(10)
+      @items = Item.order(release: :desc).limit(10)
+    end
+  
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+      @item.user_id = current_user
+      if @item.save
+        #flash[:notice] = "アイテムが登録されました。"
+        redirect_to items_path
+      else
+        render :new
+      end
+    end
+  
+    private
+    def item_params
+      params.require(:item).permit(:name, :condition_id, :rarity_id, :product, :release, :route, :get_date, :memo, :image).merge(user_id: current_user.id)
+    end
   end
-
-  def new
-    @items = Item.all
-  end
-
-  def create
-
-  end
-end
+  
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   validates :name, presence: true
+  has_one_attached :image
 end
