51aa8fb99e17a7efbb7d4bc583654305689421a6
Takagi <zkazo@gmx.com>
2023-08-10 20:10:36 +0900

    アクティブハッシュの修正

--- app/models/rarity.rb
+++ app/models/rarity.rb
@@ -1,6 +1,6 @@
 class Rarity < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '-' },
     { id: 2, name: '★☆☆☆☆' },
     { id: 3, name: '★★☆☆☆' },
     { id: 4, name: '★★★☆☆' },
